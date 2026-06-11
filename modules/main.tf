61f5eaa07ba7493b1ff34321f84e39527c2683cc
Rishik-Devops <trishik31720@gmail.com>
2026-06-11 10:16:22 +0530

    terraform aws-eks-code

--- modules/main.tf
+++ modules/main.tf
@@ -32,3 +32,13 @@ module "vpc" {
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
 }
+
+
+module "eks" {
+  source = "./modules/eks"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnet_ids
+}
